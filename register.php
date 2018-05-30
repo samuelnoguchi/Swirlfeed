f7296e3496523fb02041b9e4603a6c1ebd84a75b
root <samuelrnoguchi@gmail.com>
2018-05-30 11:26:51 -0400

    Add session variables and complete registration validation

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 $con = mysqli_connect("localhost", "root", "", "social"); //connection variable -server, username, password, table
 
 if(mysqli_connect_errno())
@@ -14,7 +15,7 @@ $em2 = ""; //email 2
 $password = ""; //password
 $password2 = ""; //password 2
 $date = ""; //Sign on date
-$error_array = ""; //holds error messages
+$error_array = array(); //holds error messages
 
 if(isset($_POST['register_button'])){
 
@@ -24,21 +25,26 @@ if(isset($_POST['register_button'])){
 	$fname = strip_tags($_POST['reg_fname']); //remove html tags
 	$fname = str_replace(' ', '', $fname); //remove spaces
 	$fname = ucfirst(strtolower($fname)); //Deals with capital letters
+	$_SESSION['reg_fname'] = $fname; //Stores first name into session variable
 
 	//last name
 	$lname = strip_tags($_POST['reg_lname']); //remove html tags
 	$lname = str_replace(' ', '', $lname); //remove spaces
 	$lname = ucfirst(strtolower($lname)); //Deals with capital letters
+	$_SESSION['reg_lname'] = $lname; //Stores last name into session variable
 
 	//email
 	$em = strip_tags($_POST['reg_email']); //remove html tags
 	$em = str_replace(' ', '', $em); //remove spaces
 	$em = ucfirst(strtolower($em)); //Deals with capital letters
+	$_SESSION['reg_email'] = $em; //Stores email into session variable
+
 
 	//email2
 	$em2 = strip_tags($_POST['reg_email2']); //remove html tags
 	$em2 = str_replace(' ', '', $em2); //remove spaces
 	$em2 = ucfirst(strtolower($em2)); //Deals with capital letters
+	$_SESSION['reg_email2'] = $em2; //Stores email2 into session variable
 
 	//Password
 	$password = strip_tags($_POST['reg_password']); //remove html tags
@@ -59,7 +65,7 @@ if(isset($_POST['register_button'])){
 			//count the number of rows returned
 
 			$num_rows = mysqli_num_rows($e_check);
-			
+
 			if($num_rows > 0) {
 				echo "Email already in use";
 			}
@@ -73,6 +79,23 @@ if(isset($_POST['register_button'])){
 		echo "Emails don't match";
 	}
 
+	if(strlen($fname) > 24 || strlen($fname) < 2) {
+		echo "Your first name must be between 2 and 25 characters";
+	}
+	if(strlen($lname) > 24 || strlen($lname) < 2) {
+		echo "Your last name must be between 2 and 25 characters";
+	}
+	if($password != $password2) {
+		echo "Your passwords do not match";
+	}
+	else {
+		if(preg_match('/[^A-Za-z0-9]/', $password)) {
+			echo "Your password can only contain characters or numbers";
+		}
+	}
+	if(strlen($password) > 30 || strlen($password) < 5) {
+		echo "Your password must be between 5 or 30 characters";
+	}
 
 
 }
@@ -89,13 +112,29 @@ if(isset($_POST['register_button'])){
 <body>
 
 	<form action="register.php" method="POST">
-		<input type="text" name="reg_fname" placeholder="First Name" required>
+		<input type="text" name="reg_fname" placeholder="First Name" value ="<?php 
+		if(isset($_SESSION['reg_fname'])) {
+			echo $_SESSION['reg_fname'];
+		}
+		?>" required>
 		<br>
-		<input type="text" name="reg_lname" placeholder="Last Name" required>
+		<input type="text" name="reg_lname" placeholder="Last Name" value ="<?php 
+		if(isset($_SESSION['reg_lname'])) {
+			echo $_SESSION['reg_lname'];
+		}
+		?>" required>
 		<br>
-		<input type="email" name="reg_email" placeholder="Email" required>
+		<input type="email" name="reg_email" placeholder="Email" value ="<?php 
+		if(isset($_SESSION['reg_email'])) {
+			echo $_SESSION['reg_email'];
+		}
+		?>" required>
 		<br>
-		<input type="email" name="reg_email2" placeholder="Confirm Email" required>
+		<input type="email" name="reg_email2" placeholder="Confirm Email" value ="<?php 
+		if(isset($_SESSION['reg_email2'])) {
+			echo $_SESSION['reg_email2'];
+		}
+		?>" required>
 		<br>
 		<input type="password" name="reg_password" placeholder="Password" required>
 		<br>
