cee9ce4ea0be367b842cab6e1e978fdd33973487
root <samuelrnoguchi@gmail.com>
2018-05-30 10:38:48 -0400

    Create php variables and begin email varification

--- register.php
+++ register.php
@@ -1,3 +1,86 @@
+<?php 
+$con = mysqli_connect("localhost", "root", "", "social"); //connection variable -server, username, password, table
+
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+//Declaring variables to prevent errors
+$fname = ""; //first name
+$lname = ""; //last name
+$em = ""; //email
+$em2 = ""; //email 2
+$password = ""; //password
+$password2 = ""; //password 2
+$date = ""; //Sign on date
+$error_array = ""; //holds error messages
+
+if(isset($_POST['register_button'])){
+
+//Registration from values
+
+	//first name
+	$fname = strip_tags($_POST['reg_fname']); //remove html tags
+	$fname = str_replace(' ', '', $fname); //remove spaces
+	$fname = ucfirst(strtolower($fname)); //Deals with capital letters
+
+	//last name
+	$lname = strip_tags($_POST['reg_lname']); //remove html tags
+	$lname = str_replace(' ', '', $lname); //remove spaces
+	$lname = ucfirst(strtolower($lname)); //Deals with capital letters
+
+	//email
+	$em = strip_tags($_POST['reg_email']); //remove html tags
+	$em = str_replace(' ', '', $em); //remove spaces
+	$em = ucfirst(strtolower($em)); //Deals with capital letters
+
+	//email2
+	$em2 = strip_tags($_POST['reg_email2']); //remove html tags
+	$em2 = str_replace(' ', '', $em2); //remove spaces
+	$em2 = ucfirst(strtolower($em2)); //Deals with capital letters
+
+	//Password
+	$password = strip_tags($_POST['reg_password']); //remove html tags
+	
+	//Password2
+	$password2 = strip_tags($_POST['reg_password2']); //remove html tags
+
+	$date = date("Y-m-d"); //Current date
+
+	if($em == $em2) {
+	//check email is valid format
+		if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
+			$em = filter_var($em, FILTER_VALIDATE_EMAIL);
+
+			//Check if email exists
+			$e_check = mysqli_query($con, "SELECT email FROM users WHERE email='$em'");
+
+			//count the number of rows returned
+
+			$num_rows = mysqli_num_rows($e_check);
+			
+			if($num_rows > 0) {
+				echo "Email already in use";
+			}
+
+		}
+		else {
+			echo "Invalid format";
+		}
+	}
+	else {
+		echo "Emails don't match";
+	}
+
+
+
+}
+
+
+ ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
