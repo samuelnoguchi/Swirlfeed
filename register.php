5d43eaf8d31362ad4ac97b59967b99c73f0a4c4c
root <samuelrnoguchi@gmail.com>
2018-05-30 15:27:55 -0400

    generate usernames

--- register.php
+++ register.php
@@ -97,6 +97,26 @@ if(isset($_POST['register_button'])){
 		array_push($error_array, "Your password must be between 5 or 30 characters<br>");
 	}
 
+	if(empty($error_array)) {
+		$password = md5($password); //Encrypt password before sending to db
+
+		//generate username by concatenating first name and last name
+
+		$username = strtolower($fname . "_" . $lname);
+		$check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username = '$username'");
+
+		$i = 0;
+		//If username exists add username
+
+		while(mysqli_num_rows($check_username_query) != 0) {
+			$i++;
+			$username = $username . "_" . $i;
+			$check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username = '$username'");
+		}
+
+
+	}
+
 
 }
 
