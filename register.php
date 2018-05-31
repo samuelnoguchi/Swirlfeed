fddb5485bf38c558ff4d51301497928b92b650f3
root <samuelrnoguchi@gmail.com>
2018-05-31 09:00:04 -0400

    Add users into data base during registration

--- register.php
+++ register.php
@@ -118,6 +118,15 @@ if(isset($_POST['register_button'])){
 
 		$profile_pic = "Assets/Images/profile_pics/defaults/head_deep_blue.png";
 
+		$query = mysqli_query($con, "INSERT INTO users VALUES ('', '$fname', '$lname', '$username', '$em', '$password', '$date', '$profile_pic', '0', '0', 'no', ',')");
+
+		array_push($error_array, "<span style='color: #14C800;'> You're all set! Go login</span><br>");
+		//Clear session variables
+
+		$_SESSION['reg_fname'] = "";
+		$_SESSION['reg_lname'] = "";
+		$_SESSION['reg_email'] = "";
+		$_SESSION['reg_email2'] = "";
 
 	}
 
@@ -177,6 +186,8 @@ if(isset($_POST['register_button'])){
 		else if(in_array("Your password must be between 5 or 30 characters<br>", $error_array)) echo "Your password must be between 5 or 30 characters<br>"; ?>
 
 		<input type="submit" name="register_button" value="Register">
+		<br>
+		<?php if(in_array("<span style='color: #14C800;'> You're all set! Go login</span><br>", $error_array)) echo "<span style='color: #14C800;'> You're all set! Go login</span><br>"; ?>
 
 
 
